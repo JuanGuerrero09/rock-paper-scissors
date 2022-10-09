a61d69afc2f238f968480848cf12292e36a78cf6
Juan David <juanguerrero09mc@gmail.com>
2022-10-09 18:50:52 -0500

    first version finished: js logic added

--- script.js
+++ script.js
@@ -1,17 +1,107 @@
-/*const options = ["rock", "paper", "scissors"]
-let win = 0
-let lose = 0
-let tie = 0
+/*
 
-function getComputerChoice(){
-    return options[Math.floor(Math.random()*options.length)]
-}
+
+
 
 
 
 
 function playRound(playerSelection, computerSelection){
     let playerPic = playerSelection.toLowerCase()
+    
+}
+
+
+function game(){
+    for (let i = 0; i < 5; i++){
+        let playerSelection = prompt("Rock, paper or scissor?")
+        const computerSelection = getComputerChoice() 
+        console.log(playRound(playerSelection, computerSelection))
+        //console.log("Number of wins: " + win)
+        //console.log("Numer of loses: " + lose)
+    }
+    console.log("The final results are: ")
+    console.log(win + " Wins")
+    console.log(lose + " Loses")
+    console.log(tie + " Ties")
+    if(win>lose){
+        console.log("You won!")
+    }
+    else if (lose>win){
+        console.log("You lose :c")
+    }
+    else{
+        console.log("It's a tie")
+    }
+}
+
+console.log(game())*/
+
+let win = 0
+let lose = 0
+let tie = 0
+let played = 0
+const options = ["rock", "paper", "scissors"]
+
+
+
+//SELECTORS
+
+
+const rock = document.querySelector('.rock')
+const paper = document.querySelector('.paper')
+const scissors = document.querySelector('.scissors')
+const userScore = document.querySelector('#user-score')
+const tieScore = document.querySelector('#tie')
+const machineScore = document.querySelector('#machine-score')
+const roundResult = document.querySelector('.round-result')
+
+
+//EVENT LISTENERS
+
+rock.addEventListener('click', playRound)
+paper.addEventListener('click', playRound)
+scissors.addEventListener('click', playRound)
+
+
+
+
+
+
+//FUNCTIONS
+
+function playGame(){
+    if (played == 5)
+    {
+        if(win>lose){
+            roundResult.innerText = ("You won!")
+        }
+        else if (lose>win){
+            roundResult.innerText = ("You lost")
+        }
+        else{
+            roundResult.innerText = ("It's a tie")
+        }
+    }
+}
+
+function playRound()
+{
+    if (played == 5)
+    {
+        if(win>lose){
+            roundResult.innerText = ("You won!")
+        }
+        else if (lose>win){
+            roundResult.innerText = ("You lost")
+        }
+        else{
+            roundResult.innerText = ("It's a tie")
+        }
+        return 0;
+    }
+    const playerPic = this.classList[1]
+    let computerSelection = getComputerChoice()
     let result = ""
     if (playerPic == "rock"){
         switch(computerSelection){
@@ -53,45 +143,30 @@ function playRound(playerSelection, computerSelection){
             
         }
     }
-    console.log("Your choice is: " + playerPic)
-    console.log("The computer choice is: " + computerSelection)
-    console.log("The result is: ")
+    roundResult.innerText = ('The machine chose ' + computerSelection + '\nthe result is: ' + result)
+    
+
     if (result == "lose"){
         lose++
-        return "You lose " + computerSelection + " beats " + playerPic + ":c"
+        //return "You lose " + computerSelection + " beats " + playerPic + ":c"
     }
     else if (result == "win"){
         win++
-        return "You won! " + playerPic+ " beats " + computerSelection + "c:"
+        //return "You won! " + playerPic+ " beats " + computerSelection + "c:"
     }
     else {
         tie++
-        return "It's a tie!"
+        //return "It's a tie!"
     }
+    userScore.innerText = win
+    tieScore.innerText = tie
+    machineScore.innerText = lose
+    played++
 }
 
 
-function game(){
-    for (let i = 0; i < 5; i++){
-        let playerSelection = prompt("Rock, paper or scissor?")
-        const computerSelection = getComputerChoice() 
-        console.log(playRound(playerSelection, computerSelection))
-        //console.log("Number of wins: " + win)
-        //console.log("Numer of loses: " + lose)
-    }
-    console.log("The final results are: ")
-    console.log(win + " Wins")
-    console.log(lose + " Loses")
-    console.log(tie + " Ties")
-    if(win>lose){
-        console.log("You won!")
-    }
-    else if (lose>win){
-        console.log("You lose :c")
-    }
-    else{
-        console.log("It's a tie")
-    }
+
+function getComputerChoice(){
+    return options[Math.floor(Math.random()*options.length)]
 }
 
-console.log(game())*/
